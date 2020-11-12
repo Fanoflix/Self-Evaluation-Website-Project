e418732b374df248df1fd33538c08375d8e982a8
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-13 00:28:31 +0500

    --- Restarting ---

--- db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="project1",
-    database="project"
-)
--- resources/teacher.py
+++
@@ -1,7 +0,0 @@
-# from models.teacher import Teacher
-# from flask_restful import Resources
-# from models impo
-
-
-# class TeacherLogin(Resources):
-#     def get():
