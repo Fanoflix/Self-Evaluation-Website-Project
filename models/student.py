92d35abc2a04eb9eb4f338048ce1fe7279d2da2e
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-13 01:09:36 +0500

    Added __repr__ for student

--- models/student.py
+++ models/student.py
@@ -8,3 +8,9 @@ class Student():
         self.no_attempts = no_attempts
         self.no_solved = no_solved
         self.rank = rank
+
+    def __repr__(self, student_id, student_name, student_email, no_attempts, no_solved, rank):
+        print("---------------------------------------------------------")
+        print(f"ID: {self.student_id} Name: {self.student_name}")
+        print(f"Email: {self.student_email} Attempts: {self.no_attempts} Solved: {self.no_solved} Rank: self{self.rank}")
+        print("---------------------------------------------------------")
