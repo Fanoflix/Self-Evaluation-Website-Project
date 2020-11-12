b17bfd9f4e3c8e81bda9ad0e2b66d24ecadee0fc
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-13 02:03:37 +0500

    Updated preferences.py

---
+++ models/preferences.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+class Preferences():
+    def __init__(self, student_id , course_name):
+        self.student_id = student_id
+        self.course_name = course_name
+    
+    def __repr__(self):
+        return f"Student ID: {self.student_id} Course Name: {self.course_name}"
