8505effde389b8b174d1bbf623a05d2384ab1f88
Abdullah Raheel <Abdullaharaheel@gmail.com>
2020-11-13 01:43:40 +0500

    Changed name

--- models/solves.py
+++ models/solves.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-class Solve():
+class Student_Solves():
     def __init__(self, student_id, assignment_id):
         self.student_id = student_id
         self.assignment_id = assignment_id
