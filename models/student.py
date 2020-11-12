dc63f96ae41c1e92604b3ef9984fc342f2231d60
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-13 01:16:59 +0500

    Updated __repr__

--- models/student.py
+++ models/student.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 class Student():
     def __init__(self, student_id, student_name, student_email, no_attempts, no_solved, rank):
         self.student_id = student_id
@@ -9,8 +10,11 @@ class Student():
         self.no_solved = no_solved
         self.rank = rank
 
-    def __repr__(self, student_id, student_name, student_email, no_attempts, no_solved, rank):
+    def info(self, student_id, student_name, student_email, no_attempts, no_solved, rank):
         print("---------------------------------------------------------")
         print(f"ID: {self.student_id} Name: {self.student_name}")
         print(f"Email: {self.student_email} Attempts: {self.no_attempts} Solved: {self.no_solved} Rank: self{self.rank}")
         print("---------------------------------------------------------")
+
+    def __repr__(self):
+        return self.info
