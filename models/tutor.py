b696dedb2b7c955ff59c467f8d264a70ac8eeb97
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-13 00:57:15 +0500

    Added teacher model

--- models/tutor.py
+++ models/tutor.py
@@ -1,4 +1,9 @@
 from flask import Flask
 
 class Tutor():
-    def __init__(self, tutorid, tutor_name, tutor_email, rating, )
+    def __init__(self, tutor_id, tutor_name, tutor_email, rating, no_reviews):
+        self.tutor_id = tutor_id
+        self.tutor_name = tutor_name
+        self.tutor_email = tutor_email
+        self.rating = rating
+        self.no_reviews = no_reviews
