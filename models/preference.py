a74927b14938f1f76b315a570149c7469b01d998
PeinZero <mahadzx@gmail.com>
2020-11-13 02:14:11 +0500

    so far so good

--- models/preference.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-class Preferences():
-    def __init__(self, student_id , course_name):
-        self.student_id = student_id
-        self.course_name = course_name
-    
-    def __repr__(self):
-        return f"student_id: {self.student_id} is doing assignment related to course: {self.course}"
