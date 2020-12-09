6bbe8918e587ca02f080f941963a9176d947772e
Ammar Bin Nasir <majidammar428@gmail.com>
2020-12-09 23:42:04 +0500

    as

--- myproject/g.py
+++
@@ -1,7 +0,0 @@
-def init():
-    global studentLoggedIn
-    global teacherLoggedIn
-    global whichStudent
-    studentLoggedIn = False
-    teacherLoggedIn = False
-    whichStudent = False
--- setupdb.py
+++
@@ -1,8 +0,0 @@
-from myproject import db
-from myproject.models import Student
-
-# db.create_all()
-
-searched = Student.query.filter_by(student_email = 'check1@gmail.com').first()
-searched.student_rank = 151
-print(searched.student_rank)
