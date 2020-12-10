61c859973f5c3c373c1196885c2ed255ebd2566e
PeinZero <mahadzx@gmail.com>
2020-12-10 21:57:15 +0500

    made assignment solving page

--- myproject/assignments/forms.py
+++ myproject/assignments/forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, Form, validators , TextAreaField,BooleanField
+from wtforms import StringField, SubmitField, PasswordField, Form, validators , TextAreaField,BooleanField,RadioField
 
 
 class SolveAssignment(FlaskForm):
-    pass
+    
+    choice = RadioField(choices=[('a1' , 'a1') , ('a2' , 'a2'), ('a3' , 'a3'), ('a4' , 'a4')])
+    submit = SubmitField('Submit')
+
 
 class DescriptionAssignment(FlaskForm):
     pass
 
 class AddAssignment(FlaskForm):
     pass
+  
 
 class UpdateAssignment(FlaskForm):
     pass
