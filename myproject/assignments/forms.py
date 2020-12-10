8c6c9cd368c4fe17014060fb4fd1ac486496d88d
PeinZero <mahadzx@gmail.com>
2020-12-10 23:07:48 +0500

    done with dynamic radio field

--- myproject/assignments/forms.py
+++ myproject/assignments/forms.py
@@ -1,11 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, Form, validators , TextAreaField,BooleanField,RadioField
 
-
 class SolveAssignment(FlaskForm):
-    
-    choice = RadioField(choices=[('a1' , 'a1') , ('a2' , 'a2'), ('a3' , 'a3'), ('a4' , 'a4')])
-    submit = SubmitField('Submit')
+    pass
 
 
 class DescriptionAssignment(FlaskForm):
--- myproject/assignments/views.py
+++ myproject/assignments/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template,redirect,url_for,flash,session
 from myproject import db,g
 from myproject.models import Student, Teacher
 from myproject.assignments.forms import SolveAssignment
+from wtforms import RadioField,SubmitField
 
 assignments_blueprint = Blueprint('assignments', __name__ , template_folder='templates/assignments')
 
@@ -30,16 +31,35 @@ def delete_assignment():
 @assignments_blueprint.route('/solve_assignment',  methods=['GET', 'POST'])
 def solve_assignment():
 
-    form = SolveAssignment()
+  
     questions = [ 'How do you do when you cant do?' ,
                 'What is your name?' , 
                 'Is Abdullah a bot? Wrong answers only' , 
                 'You done fucked up',
                 'How is testing going?']
+
+    count = 1
+    field_list = []
+    for x in range(5):
+        field = RadioField(choices=[('a1' , 'a1') , ('a2' , 'a2'), ('a3' , 'a3'), ('a4' , 'a4')])
+        setattr(SolveAssignment, 'choice' + str(count), field) 
+        field_list.append('choice'+str(count))
+        count = count + 1
+
+    submit = SubmitField("Submit")
+    setattr(SolveAssignment, 'submit', submit)
+
+    form = SolveAssignment()   
+
     if form.validate_on_submit():
-        print(form.choice.data)
-        
-    return render_template('solve_assignment.html' , form = form, teacherLoggedIn = g.teacherLoggedIn, questions = questions)
+        print(form.choice1.data)
+        print(form.choice2.data)
+        print(form.choice3.data)
+        print(form.choice4.data)
+        print(form.choice5.data)
+        #redirect to some other page
+
+    return render_template('solve_assignment.html' , form = form, teacherLoggedIn = g.teacherLoggedIn, questions = questions , field_list = field_list)
 
     
         
