c025bf0c92ab796107535d01b259658fb56ba34e
Abdullah Raheel <Abdullaharaheel@gmail.com>
2020-12-12 04:55:51 +0500

    Delete execute_migration.py

--- execute_migration.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-os.system('cmd /c"rmdir /Q /S migrations"')
-os.system('cmd /c"del myproject\data.sqlite"')
-os.system('cmd /c"flask db init"')
-os.system('cmd /c"flask db migrate"')
-os.system('cmd /c"flask db upgrade"')
