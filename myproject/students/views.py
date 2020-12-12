9637bf123c8d5b4dd4860e568950aa7da3ff5782
PeinZero <mahadzx@gmail.com>
2020-12-12 20:29:18 +0500

    made a search engine

--- myproject/students/views.py
+++ myproject/students/views.py
@@ -26,9 +26,9 @@ def signup():
 
          
         if checkTeacherUname or checkStudentUname: # Check if Uname is already taken
-            return render_template('signup.html',form=form, signupFailed1 = True)            
+            return render_template('signup.html',form=form, signupFailed1 = True,searchForm = g.searchForm)            
         elif checkTeacherEmail or checkStudentEmail:  # Check if Email is already registered
-            return render_template('signup.html',form=form, signupFailed2 = True)
+            return render_template('signup.html',form=form, signupFailed2 = True,searchForm = g.searchForm)
        
 
         if password1 != '' and password1 == password2:
@@ -40,9 +40,9 @@ def signup():
                 db.session.commit()
                 return redirect(url_for('students.login'))
         else:
-            return render_template('signup.html',form=form, signupFailed3 = True)
+            return render_template('signup.html',form=form, signupFailed3 = True ,searchForm = g.searchForm)
     
-    return render_template('signup.html',form=form )
+    return render_template('signup.html',form=form ,searchForm = g.searchForm)
 
 
 @students_blueprint.route('/login' , methods =['GET' , 'POST'])
@@ -61,9 +61,9 @@ def login():
             g.whichStudent = CheckStudent
             return redirect( url_for('index') )
         else:
-            return render_template('login.html' , form=form , loginFailed = True)
+            return render_template('login.html' , form=form , loginFailed = True,searchForm = g.searchForm)
 
-    return render_template('login.html', form=form, loginFailed = False)
+    return render_template('login.html', form=form, loginFailed = False,searchForm = g.searchForm)
             
 @students_blueprint.route('/' )
 def signout():
@@ -92,13 +92,13 @@ def profile():
         form.lname.data = ""
         form.bio.data = ""
 
-    return render_template('profile.html', form = form, studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname =g.whichStudent.student_lname, uname = g.whichStudent.student_uname, bio = g.whichStudent.student_bio )   
+    return render_template('profile.html', form = form, studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname =g.whichStudent.student_lname, uname = g.whichStudent.student_uname, bio = g.whichStudent.student_bio,searchForm = g.searchForm )   
 
 
 @students_blueprint.route('/photo' )
 def photo():
 
-    return render_template('photo.html' , studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname) 
+    return render_template('photo.html' , studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname,searchForm = g.searchForm) 
 
 @students_blueprint.route('/account',  methods =['GET' , 'POST'] )
 def account():
@@ -111,7 +111,7 @@ def account():
         if not user.check_password(form.password1.data):
             passwordChangeFailed = True
             return render_template('account.html', form = form, studentLoggedIn = g.studentLoggedIn, fname = g.whichStudent.student_fname,
-            lname = g.whichStudent.student_lname, passwordChangeFailed = passwordChangeFailed,passwordMatchFailed = passwordMatchFailed , userEmail = g.whichStudent.student_email ) 
+            lname = g.whichStudent.student_lname, passwordChangeFailed = passwordChangeFailed,passwordMatchFailed = passwordMatchFailed , userEmail = g.whichStudent.student_email,searchForm = g.searchForm ) 
             
         if form.password2.data == form.password3.data:
             user.hash_password(form.password2.data)
@@ -122,12 +122,12 @@ def account():
             passwordMatchFailed = True
 
     return render_template('account.html', form = form, studentLoggedIn = g.studentLoggedIn, fname = g.whichStudent.student_fname,
-        lname = g.whichStudent.student_lname,passwordChangeFailed = passwordChangeFailed,passwordMatchFailed = passwordMatchFailed , userEmail = g.whichStudent.student_email ) 
+        lname = g.whichStudent.student_lname,passwordChangeFailed = passwordChangeFailed,passwordMatchFailed = passwordMatchFailed , userEmail = g.whichStudent.student_email,searchForm = g.searchForm ) 
 
 @students_blueprint.route('/payment_method' )
 def payment_method():
 
-    return render_template('payment_method.html' , studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname) 
+    return render_template('payment_method.html' , studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname,searchForm = g.searchForm) 
 
 @students_blueprint.route('/privacy',  methods =['GET' , 'POST']  )
 def privacy():
@@ -140,7 +140,7 @@ def privacy():
 
             # additional code here
            
-    return render_template('privacy.html', form = form, studentLoggedIn = g.studentLoggedIn ,  fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname)  
+    return render_template('privacy.html', form = form, studentLoggedIn = g.studentLoggedIn ,  fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname,searchForm = g.searchForm)  
 
 @students_blueprint.route('/deactivate_account' ,  methods =['GET' , 'POST'] )
 def deactivate_account():
@@ -161,7 +161,7 @@ def deactivate_account():
         else:
             passwordMatchFailed = True 
 
-    return render_template('deactivate_account.html' , form = form, studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname, passwordMatchFailed = passwordMatchFailed)
+    return render_template('deactivate_account.html' , form = form, studentLoggedIn = g.studentLoggedIn , fname = g.whichStudent.student_fname, lname = g.whichStudent.student_lname, passwordMatchFailed = passwordMatchFailed,searchForm = g.searchForm)
 
 
 # @students_blueprint.route('/<uname>')
