a1b69074f6c0766394cfd11657fe3e663ef08970
Abdullah Raheel <Abdullaharaheel@gmail.com>
2020-12-16 18:38:53 +0500

    Update startup.py

--- startup.py
+++ startup.py
@@ -6,7 +6,18 @@ import time
 
 #DONT RUN THIS FILE EVERYTIME OR IT RESET THE DB
 os.system('python execute_migration.py')
-time.sleep(5)
+time.sleep(3)
 os.system('python setupdb.py')
+time.sleep(1)
 os.system('python app.py')
+
+
+
+
+
+
+
+
+
+
 
