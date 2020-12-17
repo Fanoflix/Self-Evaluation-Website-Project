821bdfbd3d76bd0cf52cbc1d00a0942c3ff63243
Abdullah Raheel <Abdullaharaheel@gmail.com>
2020-12-17 12:41:13 +0500

    Made Prefrences
    -Displaying Highest 20 rated assignments
    -Each student is being displayed his prefered assignments based on the assignments he did.
    -Just a few more checks to be added for example the student didnt do no assignments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from myproject import app
 from myproject import g
 from myproject.search.form import Searching
-from myproject.models import Assignments
+from myproject.models import Assignments, Saved_Assignemnts
 from sqlalchemy import desc
 from flask import render_template,redirect,url_for
 
@@ -13,8 +13,22 @@ def index():
     studentLoggedIn = g.studentLoggedIn
     teacherLoggedIn = g.teacherLoggedIn
     
-    all_assignments = Assignments.query.order_by(Assignments.assignment_rating.desc()).limit(20)
+  
+    all_assignments = Assignments.query.order_by(Assignments.assignment_rating.desc()).limit(20) #preference wala first kaam to howa wa hy
     # all_assignments = Assignments.query.all()
+
+    #----------------------isko test kr lena aur khain bheja nehi hy mene do what ever you want from here----------------------
+    display_assignments = []
+    if studentLoggedIn:
+        for assignment in Saved_Assignemnts.query.filter_by(student_id = g.whichStudent.id).all():
+            c_id = assignment.assignment.course.id
+            for prefed_assignment in Assignments.query.filter_by(course_id = c_id).all():
+                if prefed_assignment != assignment: #basically kiye howe assignments wapis na dikhaye usko
+                    display_assignments.append(prefed_assignment)
+        
+        print(display_assignments) #this working Perfectly so far (ab display krwa dena isko)
+    #---------------------------------------------------------------------------------------------------------------------------
+
 
     if searchForm.validate_on_submit():
         return redirect(url_for('search.searching', searched = searchForm.searched.data))
